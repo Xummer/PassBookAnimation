120f0cc87063bff4297c2ac56c02e9bc420b8d81
Xummer <xummer26@gmail.com>
2013-11-15 16:16:06 +0800

    Initial Commit

---
+++ PassBookAniDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PassBookAniDemo
+//
+//  Created by Xummer on 13-11-15.
+//  Copyright (c) 2013年 Xummer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PassBookAniDemoTests/PassBookAniDemoTests.m
@@ -0,0 +1,34 @@
+//
+//  PassBookAniDemoTests.m
+//  PassBookAniDemoTests
+//
+//  Created by Xummer on 13-11-15.
+//  Copyright (c) 2013年 Xummer. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PassBookAniDemoTests : XCTestCase
+
+@end
+
+@implementation PassBookAniDemoTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
